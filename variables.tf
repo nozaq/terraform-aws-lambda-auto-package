180dfc4c32fbccabf0da577e4e58bc3a9722a1b9
Romain <romain.philibert@worldline.com>
2020-05-03 21:42:05 +0200

    add var enable_vpc

--- variables.tf
+++ variables.tf
@@ -126,6 +126,11 @@ variable "tracing_config" {
   default = null
 }
 
+variable "enable_vpc" {
+  description = "If set to true, enable vpc"
+  type        = bool
+}
+
 variable "vpc_config" {
   description = "Provide this to allow your function to access your VPC."
   default     = null
